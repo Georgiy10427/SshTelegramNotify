9d002876fb2bd18cd33b208edd6a441a5db4afcb
GogaCoder <38399008+Georgiy10427@users.noreply.github.com>
2021-03-29 15:49:00 +0000

    Improved translation

--- auth.py
+++ auth.py
@@ -1,4 +1,5 @@
-token = input("Please, enter you bot token: ")
+print("\n--------------------------- Bot setup ---------------------------")
+token = input("Please, enter your bot token: ")
 import telebot, random, re
 
 script = """
@@ -18,26 +19,28 @@ if [ -n "$SSH_CLIENT" ]; then
 	REGION=$(cat $TMPFILE | jq '.region' | sed 's/"//g')
 	COUNTRY=$(cat $TMPFILE | jq '.country' | sed 's/"//g')
 	ORG=$(cat $TMPFILE | jq '.org' | sed 's/"//g')
-	TEXT="$DATE_EXEC: Вход пользователя ${USER} по ssh на $HOSTNAME ($IPADDR) из $IP - $ORG - $CITY, $REGION, $COUNTRY через порт $PORT"
+	TEXT="$DATE_EXEC: User Login ${USER} via SSH to $HOSTNAME ($IPADDR) from $IP - $ORG - $CITY, $REGION, $COUNTRY through port $PORT"
 	curl -s --max-time $TIMEOUT -d "chat_id=$USERID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
 	rm $TMPFILE
 fi
 """
+#Generate code and check user
 codeRand = random.randint(1000, 9999)
 bot = telebot.TeleBot(token)
-print("Please, send " + str(codeRand) + " text message to bot.\n")
+print("Please send this code bot. The code: " + str(codeRand))
 print("Wait...")
 
 @bot.message_handler(content_types=['text'])
 def start(message):
     if str(codeRand) in message.text:
+	print("Successful user check!")
         print("Your id: " + str(message.from_user.id))
-        bot.send_message(message.from_user.id, "Success!")
         f = open("/etc/profile.d/ssh-to-telegram.sh", "w")
         f.write(re.sub(r'TeleToken', str(token), re.sub(r'TeleUserId', str(message.from_user.id), script)))
         f.close()
-        #print(re.sub(r'TeleToken', str(token), re.sub(r'TeleUserId', str(message.from_user.id), script)))
-        print("Press Ctrl + C to exit...\n")
+	bot.send_message(message.from_user.id, "Successful breeding bot.")
+	print("Successful breeding bot.")
+    	print("Press Ctrl + C to exit...\n")
     exit(0)
 
 bot.polling()
